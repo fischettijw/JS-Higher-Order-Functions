674e7dda523e6a55411f1d869d2cb50a29e17782
Joseph Fischetti <fischettijw@gmail.com>
2021-03-12 14:12:34 -0500

    adding reduce

--- jsHOF.js
+++ jsHOF.js
@@ -1,4 +1,6 @@
 //https://www.youtube.com/watch?v=3LOEGS4qcRM&list=PLDlWc9AfQBfZGZXFb_1tcRKwtCavR7AfT&index=1
+//https://www.youtube.com/watch?v=h33Srr5J9nY
+//https://youtu.be/thXp0_py9X4?t=539
 
 let log = console.log;
 let clr = console.clear;
@@ -39,35 +41,138 @@ log("FEMALES");
 log(females);
 
 
-// //***MAP***
-// clr();
-// log("**MAP**");
-// log("ALL");
-// log(characters);
+//***MAP***
+clr();
+log("**MAP**");
+log("ALL");
+log(characters);
 
-// //1. Get array of all names
+//1. Get array of all names
+const names = characters.map(character => character.name);
+log("NAMES");
+log(names);
 
-// const names = characters.map(character => character.name);
-// log("NAMES");
-// log(names);
+//2. Get array of all heights
+const heights = characters.map(character => character.height);
+log("HEIGHT");
+log(heights);
 
-// //2. Get array of all heights
-// const heights = characters.map(character => character.height);
-// log("HEIGHT");
-// log(heights);
+//3. Get array of objects with just name and height properties
+const nameHeight = characters.map(character => ({
+    name: character.name,
+    height: character.height
+}));
+log("NAME/HEIGHT");
+log(nameHeight);
 
-// //3. Get array of objects with just name and height properties
-// const nameHeight = characters.map(character => ({
-//     name: character.name,
-//     height: character.height
-// }));
-// log("NAME/HEIGHT");
-// log(nameHeight);
+//4. Get array of all first names
+const firstNames = characters.map(character => character.name.split(' ')[0])
+log("FIRST NAMES");
+log(firstNames);
 
-// //4. Get array of all first names
-// const firstNames = characters.map(character => character.name.split(' ')[0])
-// log("FIRST NAMES");
-// log(firstNames);
+
+
+//***SOME***
+clr();
+log("**SOME**");
+log("ALL");
+log(characters);
+
+// 1. Is there at least one male character?
+const isOneMale = characters.some(character => character.gender == 'male');
+log("ONE MALE");
+log(isOneMale);
+
+// 2. Is there at least one character with blue eyes?
+const isOneBlueEyes = characters.some(character => character.eye_color == 'blue');
+log("ONE BLUE EYES");
+log(isOneBlueEyes);
+
+// 3. Is there at least one character taller than 210?
+const isTaller210 = characters.some(character => character.height > 210);
+log("TALLER 210");
+log(isTaller210);
+
+// 4. Is there at least one character that has mass less than 50?
+const isMassLess50 = characters.some(character => character.mass < 50);
+log("MASS < 50");
+log(isMassLess50);
+
+
+
+//***SORT***
+clr();
+log("**SORT**");
+log("ALL");
+log(characters);
+
+//1. Sort by mass
+const sortByMass = characters.sort((a, b) => a.mass - b.mass);
+log("SORT MASS");
+log(sortByMass);
+
+
+//2. Sort by height
+const sortByHeight = characters.sort((a, b) => a.height - b.height);
+log("SORT HEIGHT");
+log(sortByHeight);
+
+//3. Sort by name
+const sortByName = characters.sort((a, b) => {
+    if (a.name < b.name) return -1;
+    return 1;
+});
+log("SORT NAME");
+log(sortByName);
+
+//4. Sort by gender
+const sortByGender = characters.sort((a, b) => {
+    if (a.gender == 'female') return -1;
+    return 1;
+});
+log("SORT GENDER");
+log(sortByGender);
+
+
+
+//***REDUCE***
+clr();
+log("**REDUCE**");
+log("ALL");
+log(characters);
+
+//1. Get total mass of all characters
+const totalMass = characters.reduce((acc, curr) => {
+    return acc + curr.mass;
+}, 0);
+log("TOTAL MASS");
+log(totalMass);
+
+//2. Get total height of all characters
+const totalHeight = characters.reduce((acc, curr) => {
+    return acc + curr.height;
+}, 0);
+log("TOTAL HEIGHT");
+log(totalHeight);
+
+//3. Get total number of characters by eye color
+const numByEyeColor = characters.reduce((acc, curr) => {
+    const color = curr.eye_color;
+    if (acc[color]) {
+        acc[color]++;
+    } else {
+        acc[color] = 1;
+    }
+    return acc;
+}, {});
+log("EYE COLOR");
+log(numByEyeColor);
+
+//4. Get total number of characters in all the character names
+
+
+
+
 
 
 //***FILTER***
