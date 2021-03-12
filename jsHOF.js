35191ba8cfb34b0d35deda0d9399ce217a63729d
Joseph Fischetti <fischettijw@gmail.com>
2021-03-12 11:35:50 -0500

    added filter

--- jsHOF.js
+++ jsHOF.js
@@ -9,6 +9,15 @@ log("**FILTER**");
 log("ALL");
 log(characters);
 //1. Get characters with mass greater than 100
+
+const massGreater100_ = characters.filter(massGreater_);
+log("MASS_");
+log(massGreater100_);
+
+function massGreater_(char) {
+    return char.mass > 100;
+}
+
 const massGreater100 = characters.filter((character) => { return character.mass > 100; });
 // const massGreater100 = characters.filter(character => character.mass > 100)
 log("MASS");
@@ -30,34 +39,35 @@ log("FEMALES");
 log(females);
 
 
-//***MAP***
-clr();
-log("**MAP**");
-log("ALL");
-log(characters);
+// //***MAP***
+// clr();
+// log("**MAP**");
+// log("ALL");
+// log(characters);
 
-//1. Get array of all names
-const names = characters.map(character => character.name);
-log("NAMES");
-log(names);
+// //1. Get array of all names
 
-//2. Get array of all heights
-const heights = characters.map(character => character.height);
-log("HEIGHT");
-log(heights);
+// const names = characters.map(character => character.name);
+// log("NAMES");
+// log(names);
 
-//3. Get array of objects with just name and height properties
-const nameHeight = characters.map(character => ({
-    name: character.name,
-    height: character.height
-}));
-log("NAME/HEIGHT");
-log(nameHeight);
+// //2. Get array of all heights
+// const heights = characters.map(character => character.height);
+// log("HEIGHT");
+// log(heights);
 
-//4. Get array of all first names
-const firstNames = characters.map(character => character.name.split(' ')[0])
-log("FIRST NAMES");
-log(firstNames);
+// //3. Get array of objects with just name and height properties
+// const nameHeight = characters.map(character => ({
+//     name: character.name,
+//     height: character.height
+// }));
+// log("NAME/HEIGHT");
+// log(nameHeight);
+
+// //4. Get array of all first names
+// const firstNames = characters.map(character => character.name.split(' ')[0])
+// log("FIRST NAMES");
+// log(firstNames);
 
 
 //***FILTER***
